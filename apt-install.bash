e379fed3aa33b2e8cc1ddecf5ca0c2587c620423
Hirotaka Motai <hiromotai@gmail.com>
2020-12-07 17:05:57 +0900

    sh: add tun/tap permission before execution of QEMU
    note: a Qemu user has to belong to the KVM and netdev group.
    
    Signed-off-by: Hirotaka Motai <hiromotai@gmail.com>

--- apt-install.bash
+++ apt-install.bash
@@ -3,3 +3,5 @@
 sudo apt install \
     qemu-kvm \
     qemu-system-x86 \
+
+sudo usermod -a -G kvm,netdev $(id -un)
--- start-qemu.bash
+++ start-qemu.bash
@@ -4,7 +4,11 @@ create_vhdd() {
     qemu-img create -f qcow2 vhdd.qcow2 4G
 }
 
+sudo tunctl -t tap0 -g netdev
+
 qemu-system-x86_64 \
     -nodefaults \
     -readconfig qemu-pcie-virtio-graphic.cfg \
     -monitor stdio -S
+
+sudo tunctl -d tap0
