3702be8c90927382846f062839e43ff8b4fa145b
Hirotaka Motai <hiromotai@gmail.com>
2020-12-08 09:38:01 +0900

    sh: add command option to write down current config
    Signed-off-by: Hirotaka Motai <hiromotai@gmail.com>

--- start-qemu.bash
+++ start-qemu.bash
@@ -3,11 +3,14 @@
 CFG=qemu-pcie-emulated-graphic.cfg
 #CFG=qemu-pcie-virtio-graphic.cfg
 
+#UPDATE_CONFIG="-writeconfig qemu-dirty.cfg"
+
 sudo tunctl -t tap0 -g netdev
 
 qemu-system-x86_64 \
     -nodefaults \
     -readconfig ${CFG} \
+    $UPDATE_CONFIG \
     -monitor stdio -S
 
 sudo tunctl -d tap0
