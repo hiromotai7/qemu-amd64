d01a5d732fd5a88af5b81cec19e203a2c5f2287b
Hirotaka Motai <hirotaka.motai@miraclelinux.com>
2024-02-20 10:37:51 +0900

    qemu: change working directory
    Signed-off-by: Hirotaka Motai <hirotaka.motai@miraclelinux.com>

--- start-qemu.bash
+++ start-qemu.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 SCRIPT=$(readlink -f $0)
-cd ${SCRIPT%/*}
+DIR0=$(cd ${SCRIPT%/*}; pwd)
+cd ${DIR0}
 
 #CFG=qemu-pcie-emulated-graphic.cfg
 CFG=qemu-pcie-virtio-graphic.cfg
@@ -13,7 +14,7 @@ echo "please start rdesktop after 5sec"
 
 qemu-system-x86_64 \
     -nodefaults \
-    -acpitable file=./my-msdm.bin \
+    -acpitable file=my-msdm.bin \
     -readconfig ${CFG} \
     $UPDATE_CONFIG \
     $QDISPLAY \
