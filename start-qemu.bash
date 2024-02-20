0553e377e55e924d6495580613078dd60c2ab96f
Hirotaka Motai <hirotaka.motai@miraclelinux.com>
2024-02-20 10:30:57 +0900

    add remote display
    Signed-off-by: Hirotaka Motai <hirotaka.motai@miraclelinux.com>

--- start-qemu.bash
+++ start-qemu.bash
@@ -5,13 +5,16 @@ cd ${SCRIPT%/*}
 
 #CFG=qemu-pcie-emulated-graphic.cfg
 CFG=qemu-pcie-virtio-graphic.cfg
+QDISPLAY="-display vnc=127.0.0.1:0"
 
 UPDATE_CONFIG="-writeconfig qemu-dirty.cfg"
 
+echo "please start rdesktop after 5sec"
 
 qemu-system-x86_64 \
     -nodefaults \
     -acpitable file=./my-msdm.bin \
     -readconfig ${CFG} \
     $UPDATE_CONFIG \
+    $QDISPLAY \
     -monitor stdio -S $*
