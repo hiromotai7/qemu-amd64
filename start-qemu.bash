86ac262ea67a9b8f209509871668fca9a171de36
Hirotaka Motai <hirotaka.motai@miraclelinux.com>
2025-01-22 17:13:49 +0900

    sh: add cpu model
    - The default cpu model is "qemu64" if not set.
    - "qemu64" is as same compatibility as x86-64-v2 and some package such
      as "glibc" does not work on this cpu unfortunately.
    - Changed default cpu model to "host".
    
    Signed-off-by: Hirotaka Motai <hirotaka.motai@miraclelinux.com>

--- start-qemu.bash
+++ start-qemu.bash
@@ -8,6 +8,16 @@ cd ${DIR0}
 CFG=qemu-pcie-virtio-graphic.cfg
 QDISPLAY="-display vnc=127.0.0.1:0"
 
+## CPU MODEL (see: qemu-cpu-models.html)
+#CPU_HOST="host"
+#CPU_INTEL2015="Skylake-Client"
+#CPU_INTEL2014="Broadwel"
+#CPU_INTEL2013="Haswell"
+#CPU_AMD2017="EPYC"
+#CPU_AMD2012="Opteron_G5"
+#CPU_AMD2011="Opteron_G4"
+CPU_MODEL="host"
+
 ## readonly, written files are in tmpfs
 SNAPSHOT="-snapshot"
 
@@ -22,6 +32,7 @@ echo "please start rdesktop after 5sec"
 
 qemu-system-x86_64 \
     -nodefaults \
+    -cpu ${CPU_MODEL} \
     -acpitable file=my-msdm.bin \
     -readconfig ${CFG} \
     $UPDATE_CONFIG \
